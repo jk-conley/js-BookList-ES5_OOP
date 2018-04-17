2c203c294603934f7a3e9e2fbf5c3b8c06fcfae8
James_K_Conley <jkconleydec2012@gmail.com>
2018-04-16 19:39:39 -0700

    Add clear and add methods to UI. Add method features to form's listener

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*=========
+VARIABLES
+=========*/
+const bookForm = document.getElementById('book-form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const isbn = document.getElementById('isbn');
+
 /*===============
 CONSTRUCTORS
 ===============*/
@@ -10,12 +18,34 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function UI() { }
+function UI() {
 
-/*=========
-VARIABLES
-=========*/
-const bookForm = document.getElementById('book-form');
+  // add book to list method
+  UI.prototype.addBookToList = function (book) {
+
+    // get table body
+    const list = document.getElementById('book-list');
+    // create table row
+    const row = document.createElement('tr');
+    // insert table data
+    row.innerHTML = `<td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td><a href="#" class="delete">X</a></td>`;
+
+    // append row
+    list.appendChild(row);
+
+  }
+
+  // clear field method
+  UI.prototype.clearFields = function () {
+    title.value = '';
+    author.value = '';
+    isbn.value = '';
+  }
+
+}
 
 /*================
 EVENT LISTENERS
@@ -23,6 +53,17 @@ EVENT LISTENERS
 
 // Listen for Submit
 bookForm.addEventListener('submit', function (e) {
-  console.log('Test');
+
+  // instance of book object
+  const book = new Book(title.value, author.value, isbn.value);
+
+  // instance of UI object
+  const ui = new UI();
+
+  // add the book
+  ui.addBookToList(book);
+
+  // clear form fields
+  ui.clearFields();
   e.preventDefault();
 });
